b234704a0a40651bc0d2a549400ab498eef5d005
Ritik Sahoo <ritiksahoo380@gmail.com>
2026-04-21 14:07:22 +0530

    review

---
+++ src/components/Review.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import data from "./data";
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+
+  const { id, name, job, image, text } = data[index];
+
+  return (
+    <div className="Review">
+      <img src={image} alt={name} className="person-img" />
+
+      <h4 id={`author-${id}`} className="author">
+        {name}
+      </h4>
+
+      <p className="job">{job}</p>
+
+      <p className="info">{text}</p>
+
+      <button
+        onClick={() => {
+          setIndex((prev) =>  prev > 0 ? prev - 1 : data.length - 1 );
+        }}
+      >
+        Previous
+      </button>
+      <button
+        onClick={() => {
+          setIndex((prev) =>  prev < data.length-1 ? prev +1 : 0 );
+        }}
+      >
+        Next
+      </button>
+      <button
+        onClick={() => {
+          let randomIndex = Math.floor(Math.random() * data.length);
+
+          setIndex(randomIndex);
+        }}
+      >
+        Random
+      </button>
+    </div>
+  );
+};
+
+export default Review;
